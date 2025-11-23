c87a28186a62c27b67818600c6132eb0bacfdfcb
lil053004-bit <lil053004@gmail.com>
2025-11-23 20:33:25 +0800

    Updated NewHome.tsx

--- src/pages/NewHome.tsx
+++ src/pages/NewHome.tsx
@@ -353,31 +353,6 @@ export default function NewHome() {
 
         {stockData && diagnosisState === 'initial' && (
           <>
-            <div className="flex justify-center px-4 my-3">
-              <div className="max-w-lg w-full">
-                <button
-                  onClick={runDiagnosis}
-                  disabled={!hasRealData}
-                  className="relative group disabled:opacity-50 disabled:cursor-not-allowed w-full transform transition-all duration-300 hover:scale-105 active:scale-95"
-                >
-                  <div
-                    className="relative w-full h-24 bg-cover bg-center bg-no-repeat flex items-center justify-center"
-                    style={{
-                      backgroundImage: 'url(/button.png)',
-                      backgroundSize: 'contain',
-                      backgroundPosition: 'center'
-                    }}
-                  >
-                    <div className="relative flex items-center justify-center px-8">
-                      <span className="font-bold text-sm text-white drop-shadow-[0_2px_4px_rgba(0,0,0,0.8)]">
-                        {stockData.info.name ? `【${stockData.info.name}】の情報を表示` : '銘柄情報を表示'}
-                      </span>
-                    </div>
-                  </div>
-                </button>
-              </div>
-            </div>
-
             <SplitStockCard
               info={stockData.info}
               latestPrice={stockData.prices[0]}
